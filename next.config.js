a336d294f29a83ddac6ee5fa1cf96e294e587ccc
Joshua Rendaje <joshua.rendaje.s@southlandcollege.edu.ph>
2026-01-16 00:03:31 +0800

    Disable Sentry auth token requirement for production builds

--- next.config.js
+++ next.config.js
@@ -6,13 +6,14 @@ const nextConfig = {
 }
 
 module.exports = withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
+  org: process.env.SENTRY_ORG || "",
+  project: process.env.SENTRY_PROJECT || "",
+  authToken: process.env.SENTRY_AUTH_TOKEN || "",
+  silent: true,
+  widenClientFileUpload: false,
   transpileClientSDK: true,
   tunnelRoute: "/monitoring",
   hideSourceMaps: true,
-  dryRun: true,
+  dryRun: !process.env.SENTRY_AUTH_TOKEN,
+  disableClientWebVitalsReporting: true,
 });
